88d53fb22fccbeba3bc4ae7ad9317c9a7c1c3245
ramcharan-madasu <ramcharan.madasu@icloud.com>
2023-12-07 02:38:38 +0530

    Initial Commit

---
+++ TodoApp-ios/TodoApp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_iosApp.swift
+//  TodoApp-ios
+//
+//  Created by Ramcharan Madasu on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
